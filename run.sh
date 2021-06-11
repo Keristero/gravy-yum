c0b0effd955b284e0ef32a9165d26239830d96e1
Keristero <keristero@gmail.com>
2021-06-12 00:28:40 +0100

    fix run.sh add ignore

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 cd Scriptable-OpenNetBattle-Server
 git pull
 cargo build --release
-cp ./target/release/net_battle_server ../onb-server
-cd ../onb-server
-chmod +rwx ./run.sh
+cp ./target/release/net_battle_server ../
+cd ../
+chmod +rwx ./net_battle_server
 ./net_battle_server
