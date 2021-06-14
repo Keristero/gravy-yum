0109415d6f9722422323a5190ea03d88a0bc0633
Keristero Kerivinix <Keristero@gmail.com>
2021-06-14 22:21:46 +1200

    add lev-beast

---
+++ assets/landings/lev-beast-64-65.tsx
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="lev-beast-64-65" tilewidth="64" tileheight="65" tilecount="3" columns="3">
+ <image source="lev-beast-64-65.png" width="192" height="65"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="1000"/>
+   <frame tileid="1" duration="1000"/>
+   <frame tileid="2" duration="1000"/>
+   <frame tileid="1" duration="1000"/>
+  </animation>
+ </tile>
+</tileset>
--- scripts/landings.lua
+++ scripts/landings.lua
@@ -53,9 +53,237 @@ local fall_in_animation = {
     end
 }
 
+local lev_beast_in_animation = {
+    --these offsets will modify the warp landing location so that the player can animate from their spawn location nicely
+    pre_animation_offsets={
+        x=0,
+        y=0,
+        z=0
+    },
+    lock_camera_on_landing=false,
+    animate=function(player_id)
+        local player_pos = Net.get_player_position(player_id)
+        local area_id = Net.get_player_area(player_id)
+        
+        local lev_beast_id = "lev_beast"..player_id
+        Net.create_bot(lev_beast_id, {
+            texture_path = "/server/assets/landings/lev-beast-64-65.png",
+            animation_path = "/server/assets/landings/lev-beast-64-65.animation",
+            area_id = area_id,
+            x = player_pos.x,
+            y = player_pos.y-5,
+            z = player_pos.z+5
+        })
+
+        local beast_z_offset = 3
+        local seconds_arriving = 3
+        local seconds_here = 1
+        local seconds_leaving = 3
+
+        local player_keyframes = {{
+            properties={{
+                property="Y",
+                value=player_pos.y-3,
+            },{
+                property="Z",
+                value=player_pos.z+17
+            }},
+            duration=0
+        }}
+        player_keyframes[#player_keyframes+1] = {
+            properties={{
+                property="Y",
+                ease="Out",
+                value=player_pos.y
+            },{
+                property="Z",
+                ease="Out",
+                value=player_pos.z
+            }},
+            duration=seconds_arriving
+        }
+
+        local beast_keyframes = {{
+            properties={{
+                property="Y",
+                value=player_pos.y-3,
+            },{
+                property="Z",
+                value=player_pos.z+17+beast_z_offset
+            }},
+            duration=0
+        }}
+        beast_keyframes[#beast_keyframes+1] = {
+            properties={{
+                property="Y",
+                ease="Out",
+                value=player_pos.y
+            },{
+                property="Z",
+                ease="Out",
+                value=player_pos.z+beast_z_offset
+            }},
+            duration=seconds_arriving
+        }
+        beast_keyframes[#beast_keyframes+1] = {
+            properties={{
+                property="Y",
+                value=player_pos.y
+            },{
+                property="Z",
+                value=player_pos.z+beast_z_offset
+            }},
+            duration=seconds_here
+        }
+        beast_keyframes[#beast_keyframes+1] = {
+            properties={{
+                property="Y",
+                ease="In",
+                value=player_pos.y+3
+            },{
+                property="Z",
+                ease="In",
+                value=player_pos.z+17+beast_z_offset
+            }},
+            duration=seconds_leaving
+        }
+        Net.animate_player(player_id, "IDLE_DL",true)
+        Net.animate_player_properties(player_id, player_keyframes)
+        Net.animate_bot_properties(lev_beast_id, beast_keyframes)
+        delay.seconds(function ()
+            local sound_path = '/server/assets/landings/lev-bus-arrive.ogg'
+            Net.play_sound_for_player(player_id, sound_path)
+        end,0.1)
+        delay.seconds(function ()
+            local sound_path = '/server/assets/landings/lev-bus-leave.ogg'
+            Net.play_sound_for_player(player_id, sound_path)
+            Net.unlock_player_input(player_id)
+        end,seconds_arriving+seconds_here)
+        delay.seconds(function ()
+            Net.remove_bot(lev_beast_id)
+        end,seconds_arriving+seconds_here+seconds_leaving)
+    end
+}
+
+local lev_beast_out_animation = {
+    --these offsets will modify the warp landing location so that the player can animate from their spawn location nicely
+    pre_animation_offsets={
+        x=0,
+        y=0,
+        z=0
+    },
+    lock_camera_on_landing=false,
+    leave_animation_duration = 7,
+    animate=function(player_id)
+        local player_pos = Net.get_player_position(player_id)
+        local area_id = Net.get_player_area(player_id)
+        
+        local lev_beast_id = "lev_beast"..player_id
+        Net.create_bot(lev_beast_id, {
+            texture_path = "/server/assets/landings/lev-beast-64-65.png",
+            animation_path = "/server/assets/landings/lev-beast-64-65.animation",
+            area_id = area_id,
+            x = player_pos.x,
+            y = player_pos.y-5,
+            z = player_pos.z+5
+        })
+
+        local beast_z_offset = 3
+        local seconds_arriving = 3
+        local seconds_here = 1
+        local seconds_leaving = 3
+
+        local player_keyframes = {{
+            properties={{
+                property="Y",
+                value=player_pos.y,
+            },{
+                property="Z",
+                value=player_pos.z
+            }},
+            duration=seconds_arriving+seconds_here
+        }}
+        player_keyframes[#player_keyframes+1] = {
+            properties={{
+                property="Y",
+                ease="In",
+                value=player_pos.y+3
+            },{
+                property="Z",
+                ease="In",
+                value=player_pos.z+17
+            }},
+            duration=seconds_arriving
+        }
+
+        local beast_keyframes = {{
+            properties={{
+                property="Y",
+                value=player_pos.y-3,
+            },{
+                property="Z",
+                value=player_pos.z+17+beast_z_offset
+            }},
+            duration=0
+        }}
+        beast_keyframes[#beast_keyframes+1] = {
+            properties={{
+                property="Y",
+                ease="Out",
+                value=player_pos.y
+            },{
+                property="Z",
+                ease="Out",
+                value=player_pos.z+beast_z_offset
+            }},
+            duration=seconds_arriving
+        }
+        beast_keyframes[#beast_keyframes+1] = {
+            properties={{
+                property="Y",
+                value=player_pos.y
+            },{
+                property="Z",
+                value=player_pos.z+beast_z_offset
+            }},
+            duration=seconds_here
+        }
+        beast_keyframes[#beast_keyframes+1] = {
+            properties={{
+                property="Y",
+                ease="In",
+                value=player_pos.y+3
+            },{
+                property="Z",
+                ease="In",
+                value=player_pos.z+17+beast_z_offset
+            }},
+            duration=seconds_leaving
+        }
+        local player_mugshot = Net.get_player_mugshot(player_id)
+        local sound_path = '/server/assets/landings/lev-bus-arrive.ogg'
+        Net.play_sound(area_id, sound_path)
+        Net.animate_player(player_id, "IDLE_DL",true)
+        Net.animate_player_properties(player_id, player_keyframes)
+        Net.animate_bot_properties(lev_beast_id, beast_keyframes)
+        delay.seconds(function ()
+            Net.message_player(player_id, "AHHH! The Lev Beast is here!?", player_mugshot.texture_path, player_mugshot.animation_path)
+        end,0.1)
+        delay.seconds(function ()
+            local sound_path = '/server/assets/landings/lev-bus-leave.ogg'
+            Net.play_sound(area_id, sound_path)
+        end,seconds_arriving+seconds_here)
+        delay.seconds(function ()
+            Net.remove_bot(lev_beast_id)
+        end,seconds_arriving+seconds_here+seconds_leaving)
+    end
+}
+
 --Map animation functions to text labels
 local special_animations = {
-    fall_in = fall_in_animation
+    fall_in = fall_in_animation,
+    lev_beast_in = lev_beast_in_animation,
+    lev_beast_out = lev_beast_out_animation
 }
 
 function doAnimationForWarp(player_id,animation_name)
@@ -121,6 +349,25 @@ function handle_player_request(player_id, data)
         end
     end
     print('[Landings] no landing for '..data)
+end
+
+function handle_object_interaction(player_id, object_id)
+    local area_id = Net.get_player_area(player_id)
+    local warp_object = Net.get_object_by_id(area_id,object_id)
+    if warp_object.type ~= "Lev Beast Warp" then
+        return
+    end
+    local warp_properties = warp_object.custom_properties
+    if warp_properties.Address and warp_properties.Port then
+        local warp_out = warp_properties.WarpOut == "true"
+        local data = warp_properties.Data
+        if warp_properties.LeaveAnimation then
+            doAnimationForWarp(player_id,warp_properties.LeaveAnimation)
+        end
+        delay.seconds(function ()
+            Net.transfer_server(player_id, warp_properties.Address, warp_properties.Port, warp_out, data)
+        end,special_animations[warp_properties.LeaveAnimation].leave_animation_duration)
+    end
 end
 
 function handle_player_join(player_id)
