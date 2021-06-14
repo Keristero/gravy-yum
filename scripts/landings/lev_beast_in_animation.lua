30855e5ede35f04f3adce52a1daaa349a3de52c0
Keristero Kerivinix <Keristero@gmail.com>
2021-06-15 10:59:27 +1200

    split landings animations into seperate files

---
+++ scripts/landings/lev_beast_in_animation.lua
@@ -0,0 +1,115 @@
+local delay = require('scripts/libs/delay')
+
+local lev_beast_in_animation = {
+    --these offsets will modify the warp landing location so that the player can animate from their spawn location nicely
+    pre_animation_offsets={
+        x=0,
+        y=0,
+        z=0
+    },
+    lock_camera_on_landing=false,
+    animate=function(player_id)
+        local player_pos = Net.get_player_position(player_id)
+        local area_id = Net.get_player_area(player_id)
+        
+        local lev_beast_id = "lev_beast"..player_id
+        Net.create_bot(lev_beast_id, {
+            texture_path = "/server/assets/landings/lev-beast-64-65.png",
+            animation_path = "/server/assets/landings/lev-beast-64-65.animation",
+            area_id = area_id,
+            x = player_pos.x,
+            y = player_pos.y-5,
+            z = player_pos.z+5
+        })
+
+        local beast_z_offset = 3
+        local seconds_arriving = 3
+        local seconds_here = 1
+        local seconds_leaving = 3
+
+        local player_keyframes = {{
+            properties={{
+                property="Y",
+                value=player_pos.y-3,
+            },{
+                property="Z",
+                value=player_pos.z+17
+            }},
+            duration=0
+        }}
+        player_keyframes[#player_keyframes+1] = {
+            properties={{
+                property="Y",
+                ease="Out",
+                value=player_pos.y
+            },{
+                property="Z",
+                ease="Out",
+                value=player_pos.z
+            }},
+            duration=seconds_arriving
+        }
+
+        local beast_keyframes = {{
+            properties={{
+                property="Y",
+                value=player_pos.y-3,
+            },{
+                property="Z",
+                value=player_pos.z+17+beast_z_offset
+            }},
+            duration=0
+        }}
+        beast_keyframes[#beast_keyframes+1] = {
+            properties={{
+                property="Y",
+                ease="Out",
+                value=player_pos.y
+            },{
+                property="Z",
+                ease="Out",
+                value=player_pos.z+beast_z_offset
+            }},
+            duration=seconds_arriving
+        }
+        beast_keyframes[#beast_keyframes+1] = {
+            properties={{
+                property="Y",
+                value=player_pos.y
+            },{
+                property="Z",
+                value=player_pos.z+beast_z_offset
+            }},
+            duration=seconds_here
+        }
+        beast_keyframes[#beast_keyframes+1] = {
+            properties={{
+                property="Y",
+                ease="In",
+                value=player_pos.y+3
+            },{
+                property="Z",
+                ease="In",
+                value=player_pos.z+17+beast_z_offset
+            }},
+            duration=seconds_leaving
+        }
+        Net.animate_player(player_id, "IDLE_DL",true)
+        Net.animate_player_properties(player_id, player_keyframes)
+        Net.animate_bot_properties(lev_beast_id, beast_keyframes)
+        delay.seconds(function ()
+            local sound_path = '/server/assets/landings/lev-bus-arrive.ogg'
+            Net.play_sound_for_player(player_id, sound_path)
+        end,0.1)
+        delay.seconds(function ()
+            local sound_path = '/server/assets/landings/lev-bus-leave.ogg'
+            Net.play_sound_for_player(player_id, sound_path)
+            Net.unlock_player_input(player_id)
+        end,seconds_arriving+seconds_here)
+        delay.seconds(function ()
+            Net.remove_bot(lev_beast_id)
+        end,seconds_arriving+seconds_here+seconds_leaving)
+    end
+}
+
+return lev_beast_in_animation
---
+++ scripts/landings/main.lua
@@ -0,0 +1,139 @@
+-- Automatically adds landings for server warps to your server
+-- Just gotta add some custom properties to your Server Warps
+--   "IncomingData" (string) secret to share with the server that is linking to you; for their "Data"
+--   "Direction" (string) direction the warp will make the player walk on arrival; defaults to "Down"
+--   "WarpIn" (boolean) should the warp in animation be shown (laser from sky)
+--   "SpecialAnimation" (string) name of special animation which should play on warp in, not compatible with "WarpIn"
+--      fall_in
+local delay = require('scripts/libs/delay')
+local landings = {}
+local player_animations = {}
+local player_interactions = {}
+
+function tick(delta_time)
+    delay.on_tick(delta_time)
+end
+
+
+--Animations
+
+--Map animation functions to text labels
+local special_animations = {
+    fall_in = require('scripts/landings/fall_in_animation'),
+    lev_beast_in = require('scripts/landings/lev_beast_in_animation'),
+    lev_beast_out = require('scripts/landings/lev_beast_out_animation')
+}
+
+function doAnimationForWarp(player_id,animation_name)
+    print('[Landings] doing special animation '..animation_name)
+    Net.lock_player_input(player_id)
+    special_animations[animation_name].animate(player_id)
+end
+
+function add_landing(area_id, incoming_data, x, y, z, direction, warp_in, arrival_animation)
+    local new_landing = {
+        area_id = area_id,
+        warp_in = warp_in,
+        x = x,
+        y = y,
+        z = z,
+        pre_animation_x=x,
+        pre_animation_y=y,
+        pre_animation_z=z,
+        direction = direction,
+        arrival_animation = arrival_animation
+    }
+    landings[incoming_data] = new_landing
+    
+    print('[Landings] added landing in '..area_id)
+end
+
+local areas = Net.list_areas()
+for i, area_id in next, areas do
+    local objects = Net.list_objects(area_id)
+    for i, object_id in next, objects do
+        local object = Net.get_object_by_id(area_id, object_id)
+        local incoming_data = object.custom_properties.IncomingData
+        local arrival_animation = object.custom_properties.ArrivalAnimation
+        if object.type == "Server Warp" and incoming_data then
+            local direction = object.custom_properties.Direction or "Down"
+            local warp_in = object.custom_properties.WarpIn == "true"
+            add_landing(area_id, incoming_data, object.x+0.5, object.y+0.5, object.z, direction, warp_in,arrival_animation)
+        end
+    end
+end
+
+function handle_player_request(player_id, data)
+    print('[Landings] player '..player_id..' arrived with data: '..data)
+    if data == nil or data == "" then
+        return
+    end
+    for key, l in next, landings do
+        if data == key then
+            local entry_x = l["x"]
+            local entry_y = l["y"]
+            local entry_z = l["z"]
+            if l["arrival_animation"] then
+                local special_animation_name = l["arrival_animation"]
+                if special_animations[special_animation_name] then
+                    local special_animation = special_animations[special_animation_name]
+                    player_animations[player_id] = special_animation_name
+                    entry_x = entry_x + special_animation.pre_animation_offsets.x
+                    entry_y = entry_y + special_animation.pre_animation_offsets.y
+                    entry_z = entry_z + special_animation.pre_animation_offsets.z
+                    print('[Landings] stored arrival animation for player '..special_animation_name)
+                end
+            end
+            Net.transfer_player(player_id, l["area_id"], l["warp_in"], entry_x, entry_y, entry_z, l["direction"])
+            return
+        end
+    end
+    print('[Landings] no landing for '..data)
+end
+
+function duplicate_player_interaction(player_id,object_id)
+    if player_interactions[player_id] == object_id then
+        return true
+    else
+        player_interactions[player_id] = object_id
+        return false
+    end
+end
+
+function handle_object_interaction(player_id, object_id)
+    if duplicate_player_interaction(player_id, object_id) then
+        return
+    end
+    local area_id = Net.get_player_area(player_id)
+    local warp_object = Net.get_object_by_id(area_id,object_id)
+    if warp_object.type ~= "Interact Warp" then
+        return
+    end
+    use_warp(player_id,warp_object)
+end
+
+function use_warp(player_id,warp_object)
+    local warp_properties = warp_object.custom_properties
+    if warp_properties.Address and warp_properties.Port then
+        local warp_out = warp_properties.WarpOut == "true"
+        local data = warp_properties.Data
+        local warp_delay = 0
+        if warp_properties.LeaveAnimation then
+            doAnimationForWarp(player_id,warp_properties.LeaveAnimation)
+            warp_delay = special_animations[warp_properties.LeaveAnimation].leave_animation_duration
+        end
+        delay.seconds(function ()
+            Net.transfer_server(player_id, warp_properties.Address, warp_properties.Port, warp_out, data)
+            player_interactions[player_id] = nil
+        end,warp_delay)
+    end
+end
+
+function handle_player_join(player_id)
+    if player_animations[player_id] then
+        doAnimationForWarp(player_id,player_animations[player_id])
+        player_animations[player_id] = nil
+    end
+end
+
+print('[Landings] Loaded')
