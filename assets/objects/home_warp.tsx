d69ad2b3ca680500f1fc42ff34ce84eeec02d3c5
Keristero Kerivinix <Keristero@gmail.com>
2021-06-16 13:30:58 +1200

    add home warp collisions

--- assets/objects/home_warp.tsx
+++ assets/objects/home_warp.tsx
@@ -2,6 +2,11 @@
 <tileset version="1.5" tiledversion="1.5.0" name="home_warp" tilewidth="60" tileheight="32" tilecount="5" columns="5" objectalignment="top">
  <image source="home_warp.png" width="300" height="32"/>
  <tile id="0">
+  <objectgroup draworder="index" id="3">
+   <object id="2" x="15" y="16">
+    <polygon points="0,0 15,-8 30,0 15,8 0,0"/>
+   </object>
+  </objectgroup>
   <animation>
    <frame tileid="0" duration="100"/>
    <frame tileid="1" duration="100"/>
