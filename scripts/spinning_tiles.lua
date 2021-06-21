6409725b275ea60bc772c25ce670e0bb131bcebe
Keristero Kerivinix <Keristero@gmail.com>
2021-06-21 22:19:07 +1200

    monstrosity yum

---
+++ scripts/spinning_tiles.lua
@@ -0,0 +1,59 @@
+local tiles_to_spin = {}
+local delay_till_change = 3
+
+function StartSpinningTiles(area_id)
+    --Loop over all objects in area, spawning NPCs for each NPC type object.
+    local objects = Net.list_objects(area_id)
+    for i, object_id in next, objects do
+        local object = Net.get_object_by_id(area_id, object_id)
+        if object.type == "Spinning Conveyor" then
+            local spin = {
+                area_id=area_id,
+                x=math.floor(object.x),
+                y=math.floor(object.y),
+                z=math.floor(object.z)
+            }
+            tiles_to_spin[#tiles_to_spin+1] = spin
+            print('[spin] added spin tile '..#tiles_to_spin)
+            print(spin.x,spin.y,spin.z)
+        end
+    end
+end
+
+function LoadSpinTiles()
+    --for each area, load NPCS
+    local areas = Net.list_areas()
+    for i, area_id in next, areas do
+        --Add npcs to existing areas on startup
+        StartSpinningTiles(area_id)
+    end
+end
+
+function SpinTiles()
+    print('[spin] spinning')
+    for k, location in pairs(tiles_to_spin) do
+        local randomint = math.random(0, 1)
+        local randomint2 = math.random(0, 1)
+        local flip_h = false
+        local gid = 75
+        if randomint > 0.5 then
+            flip_h = true
+        end
+        if randomint2 > 0.5 then
+            gid = 76
+        end
+        Net.set_tile(location.area_id, location.x, location.y, location.z, gid, flip_h)
+    end
+end
+
+function tick(delta_time)
+    if delay_till_change > 0 then
+        delay_till_change = delay_till_change - delta_time
+    else
+        SpinTiles()
+        delay_till_change = 1
+    end
+end
+
+LoadSpinTiles()
+print('[spin] loaded')
