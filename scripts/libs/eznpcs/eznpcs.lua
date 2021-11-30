44d70470f1897c2eebcec9b2c43162025d11cee3
jnimmo <jnimmo@electronet.co.nz>
2021-11-30 16:24:36 +1300

    wip adding time based conditions

--- scripts/libs/eznpcs/eznpcs.lua
+++ scripts/libs/eznpcs/eznpcs.lua
@@ -1,4 +1,5 @@
 local Direction = require("scripts/libs/direction")
+local helpers = require('scripts/libs/helpers')
 local math = require('math')
 
 local eznpcs = {}
@@ -332,6 +333,39 @@ function MoveNPC(npc,delta_time)
     npc.x = new_pos.x
     npc.y = new_pos.y
 
+end
+
+function get_date_table()
+    return os.date("*t")
+end
+
+function date_string_to_timestamp(date_string)
+    --expect basic cron like date format, only supporting * or specific values
+    --0 0 10 15 * * this would be on the 15th of every month at 10AM
+    --seconds, minute, hour, day, month, year
+    local current_date = os.date("*t")
+    local date_parts = helpers.split(date_string," ")
+    if #date_parts < 6 then
+        return nil
+    end
+    local date_part_keys = {"sec","min","hour","day","month","year"}
+    --everywhere that is not a * in the date string, replace time value with specified time
+    for index, value in ipairs(date_parts) do
+        if value ~= "*" then
+            local date_part_key = date_part_keys[index]
+            current_date[date_part_key] = tonumber(value)
+        end
+    end
+    return os.time{year=current_date.year, month=current_date.month, day=current_date.day, hour=current_date.hour, min=current_date.min, sec=current_date.sec}
+end
+
+function compare_date_string(date_string)
+    local timestamp_a = os.time()
+    local timestamp_b = date_string_to_timestamp()
+
+    if timestamp_a < timestamp_b then
+        
+    end
 end
 
 function NPCReachedWaypoint(npc,waypoint)
