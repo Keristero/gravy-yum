71e8413d5e9400f4f7bac0b803ce4ef1fdc9acd7
jnimmo <jnimmo@electronet.co.nz>
2021-12-06 14:40:05 +1300

    remove unlocker name option

--- scripts/libs/ezmystery.lua
+++ scripts/libs/ezmystery.lua
@@ -85,10 +85,6 @@ function try_collect_datum(player_id,object)
         return
     end
     if object.custom_properties["Locked"] == "true" then
-        local unlocker_tool_name = "Unlocker"
-        if object.custom_properties["Unlocker Name"] then
-            unlocker_tool_name = object.custom_properties["Unlocker Name"]
-        end
         Net.message_player(player_id,"The Mystery Data is locked.")
         if ezmemory.count_player_item(player_id, "Unlocker") > 0 then
             data_dialogues[player_id] = {state="say_locked",object=object}
