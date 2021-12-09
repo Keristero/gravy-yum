c0801ae7f66d2753892a66fe52a87549e5339607
Keristero Kerivinix <Keristero@gmail.com>
2021-12-10 11:39:50 +1300

    misc

--- assets/objects/coffee.tsx
+++ assets/objects/coffee.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.5.0" name="coffee" tilewidth="33" tileheight="54" tilecount="4" columns="4" objectalignment="bottom">
+<tileset version="1.5" tiledversion="1.7.2" name="coffee" tilewidth="33" tileheight="54" tilecount="4" columns="4" objectalignment="bottom">
  <tileoffset x="0" y="8"/>
  <grid orientation="isometric" width="64" height="32"/>
  <properties>
