440db063cde409de000b3b336e7defea306151ff
Keristero Kerivinix <Keristero@gmail.com>
2022-01-03 15:07:30 +1300

    remove everything extracted to submodules

--- assets/ezlibs-assets/ezfarms/plants.tsx
+++
@@ -1,6 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.5.0" name="spacing-test" tilewidth="15" tileheight="31" tilecount="105" columns="7">
- <tileoffset x="0" y="4"/>
- <grid orientation="orthogonal" width="15" height="30"/>
- <image source="plants.png" width="105" height="465"/>
-</tileset>
--- assets/ezlibs-assets/eznpcs/sheet/readme.md
+++
@@ -1,3 +0,0 @@
-The .json files are project files compatible with
-https://github.com/Keristero/spritesheet-tool
-which can be used to generate the packed .png files and .animation files
